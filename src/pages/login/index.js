656a2bca66f52269a23b5fd7d39c9a0d7731bf7b
liuzhen <liuzhen_1993@126.com>
2022-01-13 18:59:55 +0800

    test commit

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,7 +4,7 @@ class Login extends Component {
     render() {
         return (
             <div className="P-login">
-                <h1>Login page</h1>
+                <h1>Login page1</h1>
             </div>
         )
     }
